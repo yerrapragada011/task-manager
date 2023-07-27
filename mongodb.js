5b7bf8b31b3c9a9d8eacb2d1d9c4f6cfdcba95ae
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-27 16:33:09 -0400

    Inserting Documents

--- mongodb.js
+++ mongodb.js
@@ -16,9 +16,62 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-      name: 'Agasthya',
-      age: 26,
-    });
+    // db.collection('users').insertOne(
+    //   {
+    //     name: 'Agasthya',
+    //     age: 26,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert user');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection('users').insertMany(
+    //   [
+    //     {
+    //       name: 'Agasthya',
+    //       age: 26,
+    //     },
+    //     {
+    //       name: 'Sai',
+    //       age: 55,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert documents!');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection('tasks').insertMany(
+      [
+        {
+          description: 'Clean the house',
+          completed: true,
+        },
+        {
+          description: 'Renew inspection',
+          completed: false,
+        },
+        {
+          description: 'Pot plants',
+          completed: false,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert tasks');
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
