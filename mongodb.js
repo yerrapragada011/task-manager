f29296677e3ade3ed49c1bbab38a47993e6d1708
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-28 16:28:39 -0400

    Updating Documents

--- mongodb.js
+++ mongodb.js
@@ -20,40 +20,40 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection('users').findOne(
-    //   { _id: new ObjectID('64c2ca691d4226e652d81890') },
-    //   (error, user) => {
-    //     if (error) {
-    //       return console.log('Unable to fetch');
-    //     }
-
-    //     console.log(user);
-    //   }
-    // );
-
     // db.collection('users')
-    //   .find({ age: 26 })
-    //   .toArray((error, users) => {
-    //     console.log(users);
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectID('64c2d0586bb7c5e8811fcfa3'),
+    //     },
+    //     {
+    //       $inc: {
+    //         age: 1,
+    //       },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
     //   });
 
-    // db.collection('users')
-    //     .find({ age: 26 })
-    //     .count((error, count) => {
-    //     console.log(count);
-    // });
-
-    db.collection('tasks').findOne(
-      { _id: new ObjectID('64c2d3803df9e1e968a401fd') },
-      (error, task) => {
-        console.log(task);
-      }
-    );
-
     db.collection('tasks')
-      .find({ completed: false })
-      .toArray((error, tasks) => {
-        console.log(tasks);
+      .updateMany(
+        {
+          completed: false,
+        },
+        {
+          $set: {
+            completed: true,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result.modifiedCount);
+      })
+      .catch((error) => {
+        console.log(error);
       });
   }
 );
