9c58f36a4a578cc372d54c655375c6113d315b50
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-31 14:08:36 -0400

    Data Validation and Sanitization: Part 1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useUnifiedTopology: true,
@@ -9,24 +10,43 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    },
   },
 });
 
-// const me = new User({
-//   name: 'Agasthya',
-//   age: 'Mike',
-// });
+const me = new User({
+  name: '  Agasthya  ',
+  email: 'MyEMAIL@MEAD.IO    ',
+});
 
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.log('Error!', error);
-//   });
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log('Error!', error);
+  });
 
 const Task = mongoose.model('Task', {
   description: {
@@ -37,16 +57,16 @@ const Task = mongoose.model('Task', {
   },
 });
 
-const task = new Task({
-  description: 'Learn the Mongoose library',
-  completed: false,
-});
+// const task = new Task({
+//   description: 'Learn the Mongoose library',
+//   completed: false,
+// });
 
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch((error) => {
-    console.log('Error!', error);
-  });
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task);
+//   })
+//   .catch((error) => {
+//     console.log('Error!', error);
+//   });
