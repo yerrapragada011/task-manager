83834bc61a94ae9a7b94b9ae87ffdc379f0ddae2
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-08-01 14:18:21 -0400

    Resource Reading Endpoints: Part 2

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.get('/users', (req, res) => {
       res.send(users);
     })
     .catch((e) => {
-      res.status(500).send();
+      res.status(500).send(e);
     });
 });
 
@@ -43,7 +43,7 @@ app.get('/users/:id', (req, res) => {
       res.send(user);
     })
     .catch((e) => {
-      res.status(500).send();
+      res.status(500).send(e);
     });
 });
 
@@ -57,6 +57,32 @@ app.post('/tasks', (req, res) => {
     })
     .catch((e) => {
       res.status(400).send(e);
+    });
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
     });
 });
 
