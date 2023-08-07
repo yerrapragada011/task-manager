fd4fa73389999175acb043e86bd8cd30e6d1cfa1
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-08-07 15:42:21 -0400

    Paginating Data

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,6 +18,7 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 // GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
 
@@ -30,6 +31,10 @@ router.get('/tasks', auth, async (req, res) => {
       .populate({
         path: 'tasks',
         match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+        },
       })
       .execPopulate();
     res.send(req.user.tasks);
