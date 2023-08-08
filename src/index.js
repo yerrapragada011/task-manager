10ceb3bda0a3562d89a1426a50bd93ec87b2af43
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-08-08 14:27:15 -0400

    Adding Support for File Uploads

--- src/index.js
+++ src/index.js
@@ -6,17 +6,13 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   if (req.method === 'GET') {
-//     res.send('GET requests are disabled');
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   res.status(503).send('Site is currently down. Check back soon!');
-// });
+const multer = require('multer');
+const upload = multer({
+  dest: 'images',
+});
+app.post('/upload', upload.single('upload'), (req, res) => {
+  res.send();
+});
 
 app.use(express.json());
 app.use(userRouter);
@@ -25,18 +21,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-// const main = async () => {
-//   // const task = await Task.findById('64cd59ababca571688001383');
-//   // await task.populate('owner').execPopulate();
-//   // console.log(task.owner);
-
-//   const user = await User.findById('64cd58dc24e48a15ab1318cd');
-//   await user.populate('tasks').execPopulate();
-//   console.log(user.tasks);
-// };
-
-// main();
