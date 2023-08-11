6d07399322dcd6d7c887a37ff2dee14244a26f9d
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-08-11 13:44:02 -0400

    Testing an Express Application: Part 2

---
+++ tests/user.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+test('Should signup a new user', async () => {
+  await request(app)
+    .post('/users')
+    .send({
+      name: 'Agasthya',
+      email: 'agasthya@example.com',
+      password: 'MyPass777!',
+    })
+    .expect(201);
+});
